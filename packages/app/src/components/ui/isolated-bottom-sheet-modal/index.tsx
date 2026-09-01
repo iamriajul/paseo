22ae372f71cc221868085d8ef1298bb1a4cfebfd
Riajul Islam <kmriajulislami@gmail.com>
2026-09-01 02:29:12 +0000

    fork(components/ui): fork delta in components/ui

--- packages/app/src/components/ui/isolated-bottom-sheet-modal/index.tsx
+++ packages/app/src/components/ui/isolated-bottom-sheet-modal/index.tsx
@@ -65,8 +65,7 @@ export const IsolatedBottomSheetModal = forwardRef<
       enableDismissOnClose
       stackBehavior={presentation}
     >
-      <BottomSheetScope>{contextBridge ? contextBridge(children) : children}</BottomSheetScope>
-    </GorhomBottomSheetModal>
+      <BottomSheetScope>{contextBridge ? contextBridge(children) : children}</BottomSheetScope>    </GorhomBottomSheetModal>
   );
 
   return modal;
